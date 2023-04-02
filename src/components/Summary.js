a889090797cd6c72a2c40890b21869786200d2d2
sethscodeshop <seth.garlett@hotmail.com>
2023-04-02 18:31:10 -0500

    Updated to pull summary from API

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import axios from 'axios';
+import { useAuth0 } from '@auth0/auth0-react';
+import WithAuthenticationRequired from './withAuthenticationRequired';
 
 const Summary = () => {
+
+  const [summary, setSummary] = useState([]);
+  const { getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    const getSummary = async () => {
+      const token = await getAccessTokenSilently();
+      const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/summary`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Access-Control-Allow-Origin': true
+        }
+      });
+      setSummary(response.data);
+    };
+
+    getSummary();
+  }, [getAccessTokenSilently]);
+
   return (
     <section>
       <h2 className="mb-3">Summary</h2>
-      <p>
-        Experienced IT professional with a proven track record in programming,
-        project management, team leadership, and business unit relationship
-        management. Successfully provided technical expertise during the sales
-        life-cycle and collaborated with various department executives and
-        product stakeholders to develop and deliver high-quality applications
-        that demonstrate product efficiency and business impact. Adept at
-        advising clients on design and architecture for their specific business
-        needs.
-      </p>
+      <p>{summary}</p>
     </section>
   );
 };
--- src/components/TechnicalProficiency.js
+++ src/components/TechnicalProficiency.js
@@ -6,11 +6,11 @@ const TechnicalProficiency = () => {
     <section>
       <h2 className="mb-3">Technical Proficiency</h2>
       <h3 className="mb-2">Languages:</h3>
-      <p>Proficient in: C#, JavaScript(AJAX, JSON, JQUERY), T-SQL, React</p>
+      <p>Proficient in: C#, JavaScript(AJAX, JSON, JQUERY), T-SQL, React, Swift 5.7.2, Bootstrap 3.4.1-5.3</p>
       <h3 className="mb-2">Technologies/Frameworks:</h3>
       <p>
-        Salesforce, Vivun, .NET Framework 3.5/4.0+, Web API, .Net Core 3.2, .Net
-        Core 6.0, Azure DevOps, GIT, Postman, Swift
+        Salesforce, Vivun, .NET Framework 3.5/4.0+, Web API, .Net Core 3.2, .Net 6.0
+        Azure DevOps, GIT, Postman, Jira, TFS, SAML, OAuth 2.0, OIDC
       </p>
       <h3 className="mb-2">Certifications</h3>
       <p>Google Cloud Fundamentals: Core Infrastructure</p>

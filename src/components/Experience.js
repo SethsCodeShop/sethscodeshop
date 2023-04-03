d366405a695fec39f481c22bb807bf62122a6181
sethscodeshop <seth.garlett@hotmail.com>
2023-04-02 19:09:37 -0500

    Fix error

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,4 +1,4 @@
-imprt React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useAuth0 } from '@auth0/auth0-react';
 import WithAuthenticationRequired from './withAuthenticationRequired';
